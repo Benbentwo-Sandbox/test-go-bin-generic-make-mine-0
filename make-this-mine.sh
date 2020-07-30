9c6d837649c4a9daeed7f6d87d61427e8b14d8c3
Benjamin Smith <ben.smith.developer@gmail.com>
2020-07-30 15:11:16 -0700

    wip, works locally

--- make-this-mine.sh
+++ make-this-mine.sh
@@ -1,8 +1,15 @@
 if [[ "${GITHUB_WORKSPACE}" ]]; then
 		echo "Repo: ${GITHUB_REPOSITORY}"
 		cd ${GITHUB_WORKSPACE}
+		# start replacing
 		egrep -lRZ '${GITHUB_REPOSITORY}' . | grep -v Makefile | xargs -0 -l sed -i -e 's@Benbentwo@go-bin-generic@${GITHUB_REPOSITORY}/g'
-		#sed -i 's@${GITHUB_REPOSITORY}@${GITHUB_REPOSITORY}@g' go.mod
+    find . -type f \( -iname \*.mod -o -iname \*.go \) -print0 | xargs -0 sed -i "" "s@Benbentwo/go-bin-generic@${GITHUB_REPOSITORY}@g"
+		ORG=$(echo ${GITHUB_REPOSITORY} | awk -F '/' '{print $1}')
+		REPO=$(echo ${GITHUB_REPOSITORY} | awk -F '/' '{print $2}')
+		echo "ORG: ${ORG}, REPO: ${REPO}"
+		sed -i "" "s@ORG			:= Benbentwo@ORG			:= ${ORG}@g" Makefile
+		sed -i "" "s@REPO        := go-bin-generic@REPO        := ${REPO}@g" Makefile
+		sed -i "" "s@BINARY 		:= go-bin-genericc@BINARY 		:= ${REPO}@g" Makefile
 else  # someones local
 		read -r -p "Do you want to continue? [y/N] " response
 		case "$response" in
@@ -17,4 +24,10 @@ else  # someones local
 		GITHUB_REPOSITORY=$(git remote get-url origin | awk -F 'github.com/' '{print $2}' | awk -F '.git' '{print $1}')
 		echo "GITHUB REPOSITORY: ${GITHUB_REPOSITORY}"
 		find . -type f \( -iname \*.mod -o -iname \*.go \) -print0 | xargs -0 sed -i "" "s@Benbentwo/go-bin-generic@${GITHUB_REPOSITORY}@g"
+		ORG=$(echo ${GITHUB_REPOSITORY} | awk -F '/' '{print $1}')
+		REPO=$(echo ${GITHUB_REPOSITORY} | awk -F '/' '{print $2}')
+		echo "ORG: ${ORG}, REPO: ${REPO}"
+		sed -i "" "s@ORG			:= Benbentwo@ORG			:= ${ORG}@g" Makefile
+		sed -i "" "s@REPO        := go-bin-generic@REPO        := ${REPO}@g" Makefile
+		sed -i "" "s@BINARY 		:= go-bin-genericc@BINARY 		:= ${REPO}@g" Makefile
 fi
